3a0f38521110175026df4e21232e3f7ef607fe99
Luiza Rodrigues <luizars@id.uff.br>
2023-11-23 14:44:54 -0300

    project created

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          Costs
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
