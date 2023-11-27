9392d70452eba4eda458ba839d368ff7060663ab
Luiza Rodrigues <luizars@id.uff.br>
2023-11-27 14:03:30 -0300

    adding data to API

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -5,8 +5,9 @@ import Select from '../form/Select'
 import SubmitButton from '../form/SubmitButton'
 import styles from './ProjectForm.module.css'
 
-function ProjectForm({btnText}){
+function ProjectForm({handleSubmit, btnText, projectData}){
     const [categories, setCategories] = useState([])
+    const [project, setProject] = useState(projectData || {})
 
     useEffect(() => {
         fetch("http://localhost:5000/categories", {
@@ -22,19 +23,45 @@ function ProjectForm({btnText}){
         .catch((err) => console.log(err))
     }, [])
 
+    const submit = (e) => {
+        e.preventDefault()
+        handleSubmit(project)
+    }
+
+    function handleChange(e) {
+        setProject({ ...project, [e.target.name]: e.target.value })
+    }
+
+    function handleCategory(e) {
+        setProject({ ...project, category: {
+            id: e.target.value,
+            name: e.target.options[e.target.selectedIndex].text
+            } 
+        })
+    }
+
     return (
-        <form className={styles.form}>
+        <form onSubmit={submit} className={styles.form}>
             <Input 
                type="text"
                text="Nome do Projeto"
                name="name"
-               placeholder="Insira o nome do projeto" />
+               placeholder="Insira o nome do projeto"
+               handleOnChange={handleChange} 
+               value={project.name} />
             <Input 
                type="number"
                text="Orçamento do Projeto"
                name="budget"
-               placeholder="Insira o orçamento total" />
-            <Select name="category_id" text="Selecione a categoria" options={categories}/>
+               placeholder="Insira o orçamento total"
+               handleOnChange={handleChange} 
+               value={project.budget} />
+            <Select 
+               name="category_id" 
+               text="Selecione a categoria" 
+               options={categories}
+               handleOnChange={handleCategory}
+               value={project.category ? project.category.id : ''} />
             <SubmitButton text={btnText}/>
         </form>
     )
