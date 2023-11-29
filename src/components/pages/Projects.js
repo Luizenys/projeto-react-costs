6de88137b9809090ff42ad4220eed88a6f5cd911
Luiza Rodrigues <luizars@id.uff.br>
2023-11-29 13:47:11 -0300

    pulling projects from API

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -4,13 +4,32 @@ import styles from './Projects.module.css'
 import Message from "../layout/message/Message";
 import Container from '../layout/container/Container'
 import LinkButton from '../layout/linkbutton/LinkButton'
+import { useState , useEffect} from 'react';
+import ProjectCard from '../project/ProjectCard';
 
 function Projects() {
+    const [projects, setProjects] = useState([])
     const location = useLocation()
     let message = ''
     if (location.state){
         message = location.state.message
     }
+
+    useEffect(() => {
+        fetch("http://localhost:5000/projects", {
+                method: "GET",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            })
+            .then((resp) => resp.json())
+            .then((data) => {
+                console.log(data)
+                setProjects(data)
+            })
+            .catch((err) => console.log(err))
+    }, [])
+
     return(
         <div className={styles.project_container}>
             <div className={styles.title_container}>
@@ -19,7 +38,17 @@ function Projects() {
             </div>
             {message && <Message msg={message} type="success"></Message>}
             <Container customClass="start">
-                <p>Projetos...</p>
+                {projects.length > 0 &&
+                    projects.map((project) => 
+                    <ProjectCard 
+                    id={project.id}
+                    name={project.name}
+                    budget={project.budget}
+                    category={project.category?.name}
+                    key={project.id}
+                    ></ProjectCard>
+                    )
+                }
             </Container>
         </div>
     )
